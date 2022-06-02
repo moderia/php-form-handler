3f34fd411c83997acd3dc1afbc41b2ba8aff9783
Mohamed Deria <103150230+moderia@users.noreply.github.com>
2022-06-02 00:25:44 +0000

    add echo statement from replit

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <h1>PHP Test Page - Can you see this?</h1>
     <?php
     echo '<p>This is PHP!</p>';
+    ?> 
+   <p>hello from github</p>
+    <?php
+    echo '<p>This is change from repl.it</p>';
     ?>
   
   </body>
